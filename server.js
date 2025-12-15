8722f6d1aa4880cd521d4755a243b1a2ca146351
Shahab Uddin <dev.shahab92@gmail.com>
2025-12-15 18:06:11 +0500

    update

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require("dotenv");
 const connectDB = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 const doctorRoute = require('./routes/doctor.js');
 const errorHandler = require('./middleware/error.middleware.js');
 const rateLimit = require('express-rate-limit');
+// const cookieParser = require("cookie-parser"); 
 
 // Rate limiter
 const authLimiter = rateLimit({
@@ -16,32 +18,36 @@ const authLimiter = rateLimit({
 
 // Initialize app
 const app = express();
+dotenv.config();
 connectDB();
+const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors({
   origin: ["http://localhost:5173", "https://mern-stack-auth-drab.vercel.app"],
   credentials: true,
 }));
+// app.use(cookieParser());
 app.use(express.json());
 app.use("/api/v1/auth", authLimiter, authRoutes);
 app.use("/api/doctor", doctorRoute);
 
-// Root route
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Patient Authentication API',
-    version: '1.0.0',
-    availableRoutes: [
-      'POST /api/v1/auth/google-login',
-      'GET /api/v1/auth/profile',
-      'PUT /api/v1/auth/profile'
-    ]
-  });
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
 });
 
-// Error handling middleware
+const startServer = async () => {
+  try {
+    await dataBaseconnect();
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Failed to start server:", err);
+  }
+};
+
+startServer();
 app.use(errorHandler);
 
-// **Export as serverless function**
 
